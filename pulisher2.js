f044c4eb3e596b94b8ea864fd9c3b67a7ce0124b
mknszc <ozcan.camoglu@gmail.com>
2020-12-24 20:32:19 +0300

    Initial commit

---
+++ pulisher2.js
@@ -0,0 +1,23 @@
+const amqp = require("amqplib")
+
+const message = {
+    description : "bu bir test mesajıdır. tşk"
+};
+
+connect_rabbitmq();
+
+async function connect_rabbitmq() {
+    try {
+        const connection = await amqp.connect("amqp://localhost:5672");
+        const channel    = await connection.createChannel();
+        const assertion    = await channel.assertQueue("jobsQueue");
+        setInterval(() => {
+            message.description = new Date().getTime()
+            channel.sendToQueue("jobsQueue", Buffer.from(JSON.stringify(message)));
+            console.log("Mesaj", message);
+        }, 1000);
+
+    } catch (e) {
+        console.log("Error", Error);
+    }
+}
